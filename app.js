8b6fcc6512d0c598d84fcc2de9c46114f48b5975
tiagopereira14200 <tiagopereira14200@gmail.com>
2025-09-22 17:32:09 +0000

    fix: corrige erro ao tentar logar com o google

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require('passport');
 const connectDB = require('./src/config/database');
 
 const authRoutes = require('./src/routes/authRoute');
